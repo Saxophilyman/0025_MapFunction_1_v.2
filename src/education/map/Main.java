ae722b846cacf26cc804dc18e7dc54f8a4798130
saxophilyman <zd_@mail.ru>
2023-01-16 22:18:38 +0400

    try to remember

---
+++ src/education/map/Main.java
@@ -0,0 +1,26 @@
+package education.map;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<>(100);
+        for (int i = 0; i < 100; i++) {
+            list.add(random.nextInt(10) + 1);
+        }
+        repeatNCountValueInList(list, random.nextInt(10) + 1);
+    }
+
+    public static List<Integer> repeatNCountValueInList(List<Integer> currentList, int nCount) {
+        Map<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> newList = new ArrayList<>();
+        for (Integer in : currentList) {
+            map.put(in, map.getOrDefault(in, 0) + 1);
+            if (map.get(in) == nCount) {
+                newList.add(in);
+            }
+        }
+        return newList;
+    }
+}
